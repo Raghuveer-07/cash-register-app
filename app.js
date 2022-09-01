127ffbc1d083f690b3f8b57b50507c70baac6011
Raghuveer-07 <110120267+Raghuveer-07@users.noreply.github.com>
2022-09-02 05:29:13 +0530

    htm,css,js updated and done

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
-var billAmount = document.querySelector("#bill-amount");
-var CashGiven = document.querySelector("#cash-given");
-const submitButton = document.querySelector("#submit");
-var message = document.querySelector("#error-message");
+const billAmount = document.querySelector("#bill-amount");
+const CashGiven = document.querySelector("#cash-given");
+const submitButton = document.querySelector("#submit-button");
+const message = document.querySelector("#error-message");
 const noOfNotes = document.querySelectorAll(".no-of-notes");
 
 const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
 
 function hideMessage() {
+    console.log("hiding message")
     message.style.display = "none";
   }
 
 function errorMessage(msg){
+    console.log("resetting")
     message.style.display ="block";
     message.innerText=msg;
 
@@ -18,37 +20,44 @@ function errorMessage(msg){
 
 
 function ChangeHandler(cashToReturn){
-    for (i=0;i<=availableNotes.length-1;i++){
-        var notesCount = math.trunc(cashToReturn/availableNotes[i]);
+    console.log("note count is in process")
+    for (let i=0 ; i < availableNotes.length ; i++){
+        var notesCount = Math.trunc(cashToReturn/availableNotes[i]);
 
         cashToReturn = cashToReturn % availableNotes[i];
 
         noOfNotes[i].innerText=notesCount;
+
+    console.log("note count done")
 
     }
 }
 
 function cashRegisterHandler()
     {
+        console.log("clicked and function is processing")
         hideMessage();
         if (billAmount.value>0){
 
-            if (CashGiven.value>=billAmount.value){
+            if (Number(CashGiven.value)>=Number(billAmount.value)){
+                console.log("all checks done")
                 var cashToReturn = CashGiven.value  -  billAmount.value;
                 ChangeHandler(cashToReturn);
 
             }
             else{
+                console.log("cash given is less than bill")
                 errorMessage("Wanna wash the plates ????");
             }
         }
         else{
+            console.log("bill value is not correct")
             errorMessage("Invalid Bill Amount");
         }
     }
 
 
-submitButton.addEventListener("click",cashRegisterHandler());
+submitButton.addEventListener("click",cashRegisterHandler);
 
 
     
